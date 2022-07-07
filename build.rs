36ad4977938f056eaee539b1dc47b40a6f5831f6
LightQuantum <self@lightquantum.me>
2022-07-08 04:01:06 +0800

    Fix PKCS12 load order for openssl 3.0

---
+++ build.rs
@@ -0,0 +1,10 @@
+use std::env;
+
+fn main() {
+    if let Ok(v) = env::var("DEP_OPENSSL_VERSION_NUMBER") {
+        let version = u64::from_str_radix(&v, 16).unwrap();
+        if version >= 0x3_00_00_00_0 {
+            println!("cargo:rustc-cfg=openssl_3_0");
+        }
+    }
+}
